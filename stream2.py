f38c9e754115f719889cb62e90487c878b17a307
sandeeep-r <149381904+sandeeep-r@users.noreply.github.com>
2023-10-31 08:23:51 +0530

    stream2.py

--- stream2.py
+++ stream2.py
@@ -567,10 +567,11 @@ st.text('Minimize Volatility: minstd')
 st.text('Maximize Diversification: maxdiv')
 st.text('Maximize Decorrelation: maxdecorr')
 st.text('Inverse Volatility: invstd')
-st.text('Hierarchical Equal Risk Contribution (dev): eqrisk')
+st.text('Risk Parity-Equal Risk COntribution: eqrisk')
+st.text('Hierarchical Equal Risk Contribution (dev): hier')
 st.text('Note: maxsharpe and minstd allows Download of Markowitz Efficient Frontier')
 
-method = st.selectbox(label="Select Optimization Technique", options=['maxsharpe', 'minstd', 'maxdiv', 'maxdecorr', 'invstd', 'eqrisk', 'hier])
+method = st.selectbox(label="Select Optimization Technique", options=['maxsharpe', 'minstd', 'maxdiv', 'maxdecorr', 'invstd', 'eqrisk', 'hier'])
 
 cont=False
 
